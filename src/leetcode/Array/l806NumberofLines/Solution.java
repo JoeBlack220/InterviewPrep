8ee05c9b40ceb0efe04cfccc48526648c0b0cc6b
JoeBlack220 <348653205@qq.com>
2019-08-01 23:08:44 -0700

    0802-1

---
+++ src/leetcode/Array/l806NumberofLines/Solution.java
@@ -0,0 +1,13 @@
+package leetcode.Array.l806NumberofLines;
+
+public class Solution {
+    public int[] numberOfLines(int[] widths, String S) {
+        int res = 1, cur = 0;
+        for (char c : S.toCharArray()) {
+            int width = widths[c - 'a'];
+            res = cur + width > 100 ? res + 1 : res;
+            cur = cur + width > 100 ? width : cur + width;
+        }
+        return new int[] {res, cur};
+    }
+}
