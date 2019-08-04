f63ba86ef1b9e25b854a4b6ab515fc17f0a0a7e2
JoeBlack220 <348653205@qq.com>
2019-08-03 21:27:22 -0700

    0803

--- src/datastructure/TreeNode.java
+++ src/datastructure/TreeNode.java
@@ -4,5 +4,5 @@ package datastructure;
      public int val;
      public TreeNode left;
      public TreeNode right;
-     TreeNode(int x) { val = x; }
+     public TreeNode(int x) { val = x; }
  }
---
+++ src/leetcode/Tree/l298BinaryTreeLongestConsecutiveSequence/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.Tree.l298BinaryTreeLongestConsecutiveSequence;
+import datastructure.TreeNode;
+public class Solution {
+    // straightforward traverse the tree
+    public int longestConsecutive(TreeNode root) {
+        if (root == null) return 0;
+        else {
+            if(root.val == 0) return helper(root, 1, 1, 1);
+            else return helper(root, 1, -1, 1);
+        }
+    }
+
+    private int helper(TreeNode root, int max, int lastVal, int cur) {
+        if(root == null) return max;
+        // System.out.println(root.val + "." + max);
+        if(root.val == lastVal + 1) {
+            return Math.max(helper(root.left, Math.max(max, cur + 1), root.val, cur + 1),
+                    helper(root.right, Math.max(max, cur + 1), root.val, cur + 1));
+        }
+        else {
+            return Math.max(helper(root.left, max, root.val, 1),
+                    helper(root.right, max, root.val, 1));
+        }
+    }
+}
