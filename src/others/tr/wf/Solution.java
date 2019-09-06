90b429b2d8bb53f13ef890822f83c270a8110ffe
JoeBlack220 <348653205@qq.com>
2019-09-06 12:26:21 -0500

    git ignore

---
+++ src/others/tr/wf/Solution.java
@@ -0,0 +1,34 @@
+package others.tr.wf;
+
+public class Solution {
+    public int solve(int[] v) {
+        int[] prefix = new int[v.length + 1];
+
+        for(int i = 0; i < v.length; ++i) {
+            if(v[i] == 1) {
+                prefix[i + 1] = prefix[i] + 1;
+            }
+            else {
+                prefix[i + 1] = prefix[i] - 1;
+            }
+        }
+
+        int sum = prefix[v.length];
+        for(int i = 0; i <= v.length; ++i) {
+            if(prefix[i] > sum - prefix[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] empty = new int[]{};
+        int[] normal = new int[]{1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1};
+        int[] longt = new int[]{1, 1, 0, 1, 0, 0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,0,1,1,0,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1};
+        Solution sol = new Solution();
+        System.out.println(sol.solve(empty));
+        System.out.println(sol.solve(normal));
+        System.out.println(sol.solve(longt));
+    }
+}
